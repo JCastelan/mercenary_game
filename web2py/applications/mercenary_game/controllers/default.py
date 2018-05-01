f75c60a6d0957fff891824b5fd139b6eefccfdef
mcsquizzy123 <mcsquizzy123@bitbucket.org>
2018-04-30 18:33:14 -0700

    starting with hw2 stuff

--- web2py/applications/mercenary_game/controllers/default.py
+++ web2py/applications/mercenary_game/controllers/default.py
@@ -1,58 +1,135 @@
 # -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
 # -------------------------------------------------------------------------
 # This is a sample controller
-# this file is released under public domain and you can use without limitations
+# - index is the default action of any application
+# - user is required for authentication and authorization
+# - download is for downloading files uploaded in the db (does streaming)
 # -------------------------------------------------------------------------
 
-# ---- example index page ----
+
 def index():
-    response.flash = T("Hello World")
-    return dict(message=T('Welcome to web2py!'))
+	"""
+	example action using the internationalization operator T and flash
+	rendered by views/default/index.html or views/generic.html
 
-# ---- API (example) -----
+	if you need a simple wiki simply replace the two lines below with:
+	return auth.wiki()
+	"""
+	logger.info('The session is: %r' % session)
+	checklists = None
+	if auth.user is not None:
+		checklists = db((db.checklist.user_email == auth.user.email) | (db.checklist.is_public == True)).select()
+	else:
+		checklists = db(db.checklist.is_public).select()
+	return dict(checklists=checklists)
+
+
+def no_swearing(form):
+	if 'fool' in form.vars.memo:
+		form.errors.memo = T('No swearing please')
+
+def add():
+	"""Adds a checklist."""
+	form = SQLFORM(db.checklist)
+	if form.process(onvalidation=no_swearing).accepted:
+		session.flash = T("Checklist added.")
+		redirect(URL('default','index'))
+	elif form.errors:
+		session.flash = T('Please correct the info')
+	return dict(form=form)
+
 @auth.requires_login()
-def api_get_user_email():
-    if not request.env.request_method == 'GET': raise HTTP(403)
-    return response.json({'status':'success', 'email':auth.user.email})
+@auth.requires_signature()
+def delete():
+	if request.args(0) is not None:
+		q = ((db.checklist.user_email == auth.user.email) &
+			 (db.checklist.id == request.args(0)))
+		db(q).delete()
+	redirect(URL('default', 'index'))
 
-# ---- Smart Grid (example) -----
-@auth.requires_membership('admin') # can only be accessed by members of admin groupd
-def grid():
-    response.view = 'generic.html' # use a generic view
-    tablename = request.args(0)
-    if not tablename in db.tables: raise HTTP(403)
-    grid = SQLFORM.smartgrid(db[tablename], args=[tablename], deletable=False, editable=False)
-    return dict(grid=grid)
 
-# ---- Embedded wiki (example) ----
-def wiki():
-    auth.wikimenu() # add the wiki to the menu
-    return auth.wiki() 
+@auth.requires_login()
+@auth.requires_signature()
+def toggle_public():
+	if request.args(0) is not None:
+		q = ((db.checklist.user_email == auth.user.email) &
+			 (db.checklist.id == request.args(0)))
+		row = db(q).select().first()
+		if row is None:
+			session.flash = T('Not Authorized')
+			redirect(URL('default', 'index'))
+		row.is_public = not row.is_public
+		row.update_record()
+	redirect(URL('default', 'index'))
 
-# ---- Action for login/register/etc (required for auth) -----
+
+@auth.requires_login()
+def edit():
+	"""
+	- "/edit/3" it offers a form to edit a checklist.
+	'edit' is the controller (this function)
+	'3' is request.args[0]
+	"""
+	if request.args(0) is None:
+		# We send you back to the general index.
+		redirect(URL('default', 'index'))
+	else:
+		q = ((db.checklist.user_email == auth.user.email) &
+			 (db.checklist.id == request.args(0)))
+		# I fish out the first element of the query, if there is one, otherwise None.
+		cl = db(q).select().first()
+		if cl is None:
+			session.flash = T('Not Authorized')
+			redirect(URL('default', 'index'))
+		# Always write invariants in your code.
+		# Here, the invariant is that the checklist is known to exist.
+		# Is this an edit form?
+		form = SQLFORM(db.checklist, record=cl, deletable=False)
+		if form.process(onvalidation=no_swearing).accepted:
+			# At this point, the record has already been edited.
+			session.flash = T('Checklist edited.')
+			redirect(URL('default', 'index'))
+		elif form.errors:
+			session.flash = T('Please enter correct values.')
+	return dict(form=form)
+
 def user():
-    """
-    exposes:
-    http://..../[app]/default/user/login
-    http://..../[app]/default/user/logout
-    http://..../[app]/default/user/register
-    http://..../[app]/default/user/profile
-    http://..../[app]/default/user/retrieve_password
-    http://..../[app]/default/user/change_password
-    http://..../[app]/default/user/bulk_register
-    use @auth.requires_login()
-        @auth.requires_membership('group name')
-        @auth.requires_permission('read','table name',record_id)
-    to decorate functions that need access control
-    also notice there is http://..../[app]/appadmin/manage/auth to allow administrator to manage users
-    """
-    return dict(form=auth())
+	"""
+	exposes:
+	http://..../[app]/default/user/login
+	http://..../[app]/default/user/logout
+	http://..../[app]/default/user/register
+	http://..../[app]/default/user/profile
+	http://..../[app]/default/user/retrieve_password
+	http://..../[app]/default/user/change_password
+	http://..../[app]/default/user/bulk_register
+	use @auth.requires_login()
+		@auth.requires_membership('group name')
+		@auth.requires_permission('read','table name',record_id)
+	to decorate functions that need access control
+	also notice there is http://..../[app]/appadmin/manage/auth to allow administrator to manage users
+	"""
+	return dict(form=auth())
 
-# ---- action to server uploaded static content (required) ---
+
 @cache.action()
 def download():
-    """
-    allows downloading of uploaded files
-    http://..../[app]/default/download/[filename]
-    """
-    return response.download(request, db)
+	"""
+	allows downloading of uploaded files
+	http://..../[app]/default/download/[filename]
+	"""
+	return response.download(request, db)
+
+
+def call():
+	"""
+	exposes services. for example:
+	http://..../[app]/default/call/jsonrpc
+	decorate with @services.jsonrpc the functions to expose
+	supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+	"""
+	return service()
+
+
