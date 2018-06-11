b5ff01b02a1e1a2c11d3051ef78cb6ca7be83906
Joseph <jcastelan02@gmail.com>
2018-06-10 22:41:40 -0700

    minor bug fixes

--- controllers/api.py
+++ controllers/api.py
@@ -2,7 +2,7 @@
 
 def get_name():
 	if auth.user is None:
-		return response.json(dict(logged_in = false))
+		return response.json(dict(logged_in = False))
 	
 	r = db(db.auth_user.email == auth.user.email).select().first()
 	return response.json(dict(
@@ -26,6 +26,32 @@ def load_counter():
 
 # @auth.requires_signature()
 def save_counter():
+	if auth.user is None:
+		return
+	# print "save_counter!!!!!!!!!!!!!!!!!"
+	q=(db.userdb.user_email ==auth.user.email)
+	row=db(q).select().first()
+	if row is not None:
+		result = row.update_record(counter=request.vars.counter)
+		return
+	else:
+		result = db.userdb.insert()
+		return
+
+def load_resources():
+	if auth.user is None:
+		return
+	# print("load_counter!!!!!!!!!!!!!!!!!")
+	q=(db.userdb.user_email ==auth.user.email)
+	row=db(q).select().first()
+	if row is None:
+		# print( "\tdne")
+		return response.json(dict(counter=0))
+	else:
+		# print("\texists")
+		return response.json(dict(counter=row.counter))
+
+def save_resources():
 	if auth.user is None:
 		return
 	# print "save_counter!!!!!!!!!!!!!!!!!"
--- models/tables.py
+++ models/tables.py
@@ -13,22 +13,6 @@ def get_user_email():
     return auth.user.email if auth.user is not None else None
 
 
-db.define_table('checklist',
-                Field('user_email', default=get_user_email()),
-                Field('title'),
-                Field('memo', 'text'),
-                Field('updated_on', 'datetime', update=datetime.datetime.utcnow()),
-                Field('is_public', 'boolean', default=False)
-                )
-
-db.checklist.user_email.writable = False
-db.checklist.user_email.readable = False
-db.checklist.updated_on.writable = db.checklist.updated_on.readable = False
-db.checklist.id.writable = db.checklist.id.readable = False
-
-db.checklist.is_public.writable = False
-db.checklist.is_public.readable = False
-
 """THIS TABLE WAS CREATED FOR THE PROJECT"""
 db.define_table('userdb',
 				Field( 'user_email', default=get_user_email()),
@@ -50,7 +34,5 @@ db.define_table('userdb',
 
 #some db entries we might need: wood, iron, coal, worker, armyUnit, houses, etc.
 db.userdb.user_email.writable = db.userdb.user_email.readable = False
-db.checklist.updated_on.writable = db.checklist.updated_on.readable = False
-
 # after defining tables, uncomment below to enable auditing
 # auth.enable_record_versioning(db)
